4a4bda15742fc4e6a9674b77e9342204c1341d12
Gabriel Soares gomes <dev.gabrielsoares221@gmail.com>
2022-02-19 17:53:16 -0300

    feat: Criando arquivo de template

--- meu_site.py
+++ meu_site.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 # -> route: www.google.com/"route"
 # -> função: O que deverá ser exibido na página
+# -> template: Arquivo html
 
 @app.route("/")
 def index():
@@ -13,7 +14,7 @@ def index():
 
 @app.route("/contatos")
 def contatos():
-    return "Meus contatos: Email: dev.gabrielsoares221@gmail.com | Telefone: (24)981536244"
+    return "<p>Meus contatos:</p> <p>|Email: dev.gabrielsoares221@gmail.com|</p><p>|Telefone: (24)981536244|</p>"
 
 if __name__ == "__main__": #Quando rodar o arquivo direto(abrindo o .py no terminal por exemplo), irá rodar essa função
     app.run(debug=True)
